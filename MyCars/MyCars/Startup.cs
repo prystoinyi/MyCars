7dd0da6b134a639a5c742c7dd1e181ac7af8d2eb
Roman <roman.prystojnyj@gmail.com>
2018-03-29 20:19:05 +0300

    Add project

---
+++ MyCars/MyCars/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyCars.Startup))]
+namespace MyCars
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
