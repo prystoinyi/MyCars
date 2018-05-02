129e32576ee20f49bca5e8024d0a3582dcb8cf30
Roman <roman.prystojnyj@gmail.com>
2018-05-02 15:04:42 +0300

    Added method of adding a new car for user

--- MyCars/MyCars/Controllers/HomeController.cs
+++ MyCars/MyCars/Controllers/HomeController.cs
@@ -44,7 +44,7 @@ namespace MyCars.Controllers
 
         public ActionResult GetItems(int id)
         {
-            return PartialView(db.Types.Where(c => c.BrandId == id).ToList());
+            return PartialView(db.Types.Where(c => c.BrandId == id).OrderBy(x => x.Name).ToList());
         }
 
         [HttpGet]
@@ -76,6 +76,46 @@ namespace MyCars.Controllers
             }
 
             return View(NewCar);
+        }
+
+        [HttpGet]
+        public ActionResult AddNewCar(int id = 0)
+        {
+            UserInfo userinfo = db.UsersInfo.Find(id);
+            if (userinfo == null)
+            {
+                return HttpNotFound();
+            }
+
+            int selectedIndex = 1;
+            SelectList brandd = new SelectList(db.Brands.OrderBy(x => x.Name), "Id", "Name", selectedIndex);
+            ViewBag.Brands = brandd;
+            SelectList type = new SelectList(db.Types.Where(c => c.BrandId == selectedIndex).OrderBy(x => x.Name), "Id", "Name");
+            ViewBag.Types = type;
+
+            return View(userinfo);
+        }
+
+        [HttpPost]
+        public ActionResult AddNewCar(UserInfo userInfo, int? selectedModel)
+        {
+            UserInfo newUserInfo = db.UsersInfo.Find(userInfo.Id);
+
+            var types = newUserInfo.TypeModels;
+
+            if (selectedModel.HasValue)
+            {
+                var modelId = selectedModel.Value;
+                foreach (var item in db.Types.Where(co => modelId == co.Id))
+                {
+                    types.Add(item);
+                }
+            }
+
+            db.Entry(newUserInfo).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
